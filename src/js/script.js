1d50f178d686a32d1d223a1c6d939364c5074f2c
dmitrykuzan <dmitrykuzan@gmail.com>
2022-07-25 15:23:56 +0300

    trust, service

--- src/js/script.js
+++ src/js/script.js
@@ -6,10 +6,17 @@ import "./_vendor";
 // Functions
 import { mobileCheck, burger } from "./functions/";
 
+//libs
+import AOS from "aos";
+
 // Components
 // import { formValidation } from "./components/";
 
 window.addEventListener("DOMContentLoaded", () => {
   mobileCheck();
   burger();
+  AOS.init({
+    once: true,
+  });
 });
+
