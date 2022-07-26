d6ea92a50e551b60316655baacd701e1c3a2f6c6
dmitrykuzan <78803281+dmitrykuzan@users.noreply.github.com>
2022-07-26 08:02:21 +0000

    Deploying to gh-pages from @ dmitrykuzan/portfolio_creator@f8f2e71086470c438da517680f4d2452a26fd7a5 🚀

--- mail.php
+++ mail.php
@@ -33,15 +33,15 @@ try {
 
   // Настройки вашей почты
   $mail->Host       = 'smtp.gmail.com'; // SMTP сервера вашей почты
-  $mail->Username   = ''; // Логин на почте
-  $mail->Password   = ''; // Пароль на почте
+  $mail->Username   = 'dmitrykuzan@gmail.com'; // Логин на почте
+  $mail->Password   = 'iumykpawysypqndd'; // Пароль на почте
   $mail->SMTPSecure = 'ssl';
   $mail->Port       = 465;
 
-  $mail->setFrom('', 'Заявка с вашего сайта'); // Адрес самой почты и имя отправителя
+  $mail->setFrom('dmitrykuzan@gmail.com', 'Заявка с вашего сайта'); // Адрес самой почты и имя отправителя
 
   // Получатель письма
-  $mail->addAddress('');
+  $mail->addAddress('dmitrykuzan@gmail.com');
 
   // Прикрипление файлов к письму
   if (!empty($file['name'][0])) {
